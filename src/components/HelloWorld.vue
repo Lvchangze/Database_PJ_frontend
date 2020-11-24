6d3cf16d1d123bb3bd546aecf56d6935105650fc
Lvchangze <18302010026@fudan.edu.cn>
2020-11-24 15:34:56 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-button v-on:click="hhh">沈征宇永远的神</el-button>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    methods: {
+      hhh() {
+        this.axios({
+          url:  "/api/addPerson",
+          method: "post",
+          params: {
+            name:"吕昌泽"
+          }
+        }).then(res => {
+          console.log(res)
+        })
+      }
+    },
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
